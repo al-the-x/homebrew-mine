0a50a93c451bba8c3e2adf130e52e58bcd3c50a5
David Rogers <hello@al-the-x.me>
2019-08-09 21:07:54 -0400

    Correct flags to remove duped `--`

--- shiny-and-gnu.rb
+++ shiny-and-gnu.rb
@@ -7,16 +7,16 @@ class ShinyAndGnu < Formula
   # GNU utils make MacOSX feel less like BSD...
   depends_on 'coreutils'
   depends_on 'diffutils'
-  depends_on 'findutils' => '--default-names'
+  depends_on 'findutils' => 'default-names'
   depends_on 'gawk'
-  depends_on 'gnu-indent' => '--default-names'
-  depends_on 'gnu-sed' => '--default-names'
-  depends_on 'gnu-tar' => '--default-names'
-  depends_on 'gnu-which' => '--default-names'
-  depends_on 'grep' => '--default-names'
+  depends_on 'gnu-indent' => 'default-names'
+  depends_on 'gnu-sed' => 'default-names'
+  depends_on 'gnu-tar' => 'default-names'
+  depends_on 'gnu-which' => 'default-names'
+  depends_on 'grep' => 'default-names'
   depends_on 'gzip'
   depends_on 'watch'
-  depends_on 'wdiff' => '--with-gettext'
+  depends_on 'wdiff' => 'with-gettext'
   depends_on 'wget'
 
   # Thanks for the antiques, MacOSX...
@@ -27,7 +27,7 @@ class ShinyAndGnu < Formula
   depends_on 'openssh'
   depends_on 'python'
   depends_on 'rsync'
-  depends_on 'vim' => '--override-system-vi'
+  depends_on 'vim' => 'override-system-vi'
   depends_on 'zsh'
 
   # It's complicated...
